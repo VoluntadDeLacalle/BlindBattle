a0ee31081f4b71dcee1ce71e4c4c5842efc47b19
William Gavin Lacalle <digitalbros7@gmail.com>
2022-06-18 23:12:07 -0500

    Adds animator controller and parameters WIP

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -7,9 +7,16 @@ public class Gun : NetworkBehaviour
 {
     [SerializeField] private Transform shootTransform;
     public float shootDistance = 50f;
+    public int maxAmmo = 15;
+    private int currentAmmo = 0;
 
     [Header("Prefabs")]
     [SerializeField] private NetworkPrefabRef gunRicochetPrefab;
+
+    private void Awake()
+    {
+        currentAmmo = maxAmmo;
+    }
 
     private void OnDrawGizmos()
     {
@@ -21,9 +28,41 @@ public class Gun : NetworkBehaviour
             Gizmos.DrawRay(shootTransform.position, ((shootTransform.position + shootTransform.forward) - shootTransform.position).normalized * shootDistance);
         }
     }
+
+    public int GetCurrentAmmo()
+    {
+        return currentAmmo;
+    }
+
+    public void SetCurrentAmmo(int ammoAmount)
+    {
+        if (ammoAmount <= 0)
+        {
+            currentAmmo = 0;
+        }
+        else if (ammoAmount >= maxAmmo)
+        {
+            currentAmmo = maxAmmo;
+        }
+        else
+        {
+            currentAmmo = ammoAmount;
+        }
+
+    }
     
     public bool ShootGun(NetworkObject playerNetworkObject, out LagCompensatedHit raycastHit)
     {
+        if (currentAmmo <= 0)
+        {
+            raycastHit = new LagCompensatedHit { };
+            return false;
+        }
+        else
+        {
+            currentAmmo--;
+        }
+
         return Runner.LagCompensation.Raycast(shootTransform.position, ((shootTransform.position + shootTransform.forward) - shootTransform.position).normalized, shootDistance, playerNetworkObject.InputAuthority, out raycastHit, -1, HitOptions.IncludePhysX);
     }
 
